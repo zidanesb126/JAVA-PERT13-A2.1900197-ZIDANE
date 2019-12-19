3503d601990fb64282991d196f3297001a285861
Dindatr123 <dindatrirahmasari@gmail.com>
2019-12-19 19:14:05 +0700

    Pertemuan 13

---
+++ 04-MemehamiProgram/src/Pertemuan13.java
@@ -0,0 +1,9 @@
+public class Pertemuan13 {
+    public static void main(String[] args)
+    {
+        /**
+         *
+         */
+        System.out.println("Nama ku adalah , teman si ucup");
+    }
+}
---
+++ 05-OrganisasiProgramdenganPackage/src/ucup/Main.java
@@ -0,0 +1,12 @@
+package ucup;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        /**
+         * 
+         */
+        System.out.println("hello world");
+    }
+}
